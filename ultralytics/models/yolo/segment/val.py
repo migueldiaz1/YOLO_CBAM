58d235b7c2e8140c60b2fd03ca6a7d140f86e5f9
migueldiaz1 <87194086+migueldiaz1@users.noreply.github.com>
2024-04-27 11:31:39 +0200

    Update val.py

--- ultralytics/models/yolo/segment/val.py
+++ ultralytics/models/yolo/segment/val.py
@@ -65,8 +65,8 @@ class SegmentationValidator(DetectionValidator):
             "mAP50-95)",
             "Mask(P",
             "R",
-            "DICE(50)",
-            "DICE(50-95))",
+            "mAP50",
+            "mAP50-95)",
         )
 
     def postprocess(self, preds):
