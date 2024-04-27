4ff6973890b951a8a1316bdc0137f073364e2faa
migueldiaz1 <87194086+migueldiaz1@users.noreply.github.com>
2024-04-27 11:27:07 +0200

    Update metrics.py

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -129,7 +129,7 @@ def bbox_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, eps=1e-7
     return iou  # IoU
 
 
-def mask_iou(mask1, mask2, eps=1e-7):
+#def mask_iou(mask1, mask2, eps=1e-7):
     """
     Calculate masks IoU.
 
@@ -143,9 +143,27 @@ def mask_iou(mask1, mask2, eps=1e-7):
     Returns:
         (torch.Tensor): A tensor of shape (N, M) representing masks IoU.
     """
+    #intersection = torch.matmul(mask1, mask2.T).clamp_(0)
+    #union = (mask1.sum(1)[:, None] + mask2.sum(1)[None]) - intersection  # (area1 + area2) - intersection
+    #return intersection / (union + eps)
+
+
+def mask_iou(mask1, mask2, eps=1e-7):
+    """
+    Calculate masks DICE.
+
+    Args:
+        mask1 (torch.Tensor): A tensor of shape (N, n) where N is the number of ground truth objects and n is the
+                        product of image width and height.
+        mask2 (torch.Tensor): A tensor of shape (M, n) where M is the number of predicted objects and n is the
+                        product of image width and height.
+        eps (float, optional): A small value to avoid division by zero. Defaults to 1e-7.
+
+    Returns:
+        (torch.Tensor): A tensor of shape (N, M) representing masks IoU.
+    """
     intersection = torch.matmul(mask1, mask2.T).clamp_(0)
-    union = (mask1.sum(1)[:, None] + mask2.sum(1)[None]) - intersection  # (area1 + area2) - intersection
-    return intersection / (union + eps)
+    return 2*intersection / ((mask1.sum(1)[:, None] + mask2.sum(1)[None]) + eps)
 
 
 def kpt_iou(kpt1, kpt2, area, sigma, eps=1e-7):
